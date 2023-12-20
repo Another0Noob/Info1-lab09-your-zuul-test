cac3ed2e5195d70c24a89fef83265c7979257de2
Another0Noob <25Antonym02+github@gmail.com>
2023-12-20 19:38:35 +0100

    Added cellar and changed setExit

--- Room.java
+++ Room.java
@@ -1,68 +1,26 @@
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-/**
- * Class Room - a room in an adventure game.
- *
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text-based adventure game.
- *
- * A "Room" represents one location in the scenery of the game. It is
- * connected to other rooms via exits. The exits are labeled north,
- * east, south, west. For each direction, the room stores a reference
- * to the neighboring room, or null if there is no exit in that direction.
- *
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
- */
 public class Room {
     public Map<String, Room> exits = new LinkedHashMap<>();
     public String description;
 
-    /**
-     * Create a room described "description". Initially, it has
-     * no exits. "description" is something like "a kitchen" or
-     * "an open courtyard".
-     *
-     * @param description The room's description.
-     */
     public Room(String description) {
         this.description = description;
     }
 
-    /**
-     * Define the exits of this room. Every direction either leads
-     * to another room or is null (no exit there).
-     *
-     * @param north The north exit.
-     * @param east The east exit.
-     * @param south The south exit.
-     * @param west The west exit.
-     */
-    public void setExits(Room north, Room east, Room south, Room west) {
-        exits.put("north", north);
-        exits.put("east", east);
-        exits.put("south", south);
-        exits.put("west", west);
+    public void addExit(String direction, Room room) {
+        exits.put(direction, room);
     }
 
     public Room getExit(String direction) {
         return exits.get(direction);
     }
 
-    /**
-     * @return The description of the room.
-     */
     public String getDescription() {
         return description;
     }
 
-    /**
-     * Create all the rooms and link their exits together.
-     *
-     * @param game
-     * @return
-     */
     public static Room createRooms(Game game) {
         Room outside, theater, pub, lab, office;
 
@@ -74,11 +32,18 @@ public class Room {
         office = new Room("in the computing admin office");
 
         // initialize room exits
-        outside.setExits(theater, lab, pub, null);
-        theater.setExits(null, null, null, outside);
-        pub.setExits(outside, null, null, null);
-        lab.setExits(office, null, null, outside);
-        office.setExits(null, lab, null, null);
+        outside.addExit("north", theater);
+        outside.addExit("east", lab);
+        outside.addExit("south", pub);
+
+        theater.addExit("west", outside);
+
+        pub.addExit("north", outside);
+
+        lab.addExit("west", outside);
+        lab.addExit("south", office);
+
+        office.addExit("north", lab);
 
         return outside; // start game outside
     }
